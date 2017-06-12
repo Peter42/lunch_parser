2f3c9451207d7652986263ef6c3ab923c7d9786b
Philipp Stehle <philipp.stehle@sap.com>
2017-06-12 12:16:49 +0200

    Fix KITLunchMenuProvider Date Bug. Closes #4

--- kit-mensa/src/main/java/de/philipp1994/lunch/kit/KITLunchMenuProvider.java
+++ kit-mensa/src/main/java/de/philipp1994/lunch/kit/KITLunchMenuProvider.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.IsoFields;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -20,28 +22,23 @@ import de.philipp1994.lunch.common.tools.Cache;
 
 public class KITLunchMenuProvider extends AbstractLunchMenuProvider {
 
-	private static final URI FALLBACK_URL;
-	private static final URI URL;
 	private static final Map<LocalDate, LunchMenu> cache = Cache.getSynchronizedCache(7);
 	private static final String MENSA_NAME = "KIT Mensa";
-
-	static {
-		URI t = null;
+	
+	private static URI getURI(LocalDate date) throws IOException {
 		try {
-			t = new URI("http://mensa.akk.uni-karlsruhe.de/?DATUM=heute&uni=1&schnell=1");
+			return new URI("http://www.sw-ka.de/de/essen/?view=ok&STYLE=popup_plain&c=adenauerring&p=1&kw=" + date.get(IsoFields.WEEK_OF_WEEK_BASED_YEAR));
 		} catch (URISyntaxException e) {
-			e.printStackTrace();
-		} finally {
-			FALLBACK_URL = t;
+			throw new IOException(e);
 		}
-		
-		t = null;
+	}
+	
+	private static URI getFallbackURI(LocalDate date) throws IOException {
+		final String formattedDate = DateTimeFormatter.ofPattern("dd.MM.yyyy").format(date);
 		try {
-			t = new URI("http://www.sw-ka.de/de/essen/?view=ok&STYLE=popup_plain&c=adenauerring&p=1");
+			return new URI("http://mensa.akk.uni-karlsruhe.de/?DATUM=" + formattedDate + "&uni=1&schnell=1");
 		} catch (URISyntaxException e) {
-			e.printStackTrace();
-		} finally {
-			URL = t;
+			throw new IOException(e);
 		}
 	}
 
@@ -51,15 +48,22 @@ public class KITLunchMenuProvider extends AbstractLunchMenuProvider {
 		if(cache.containsKey(date)){
 			return cache.get(date);
 		}
-		
+
 		try {
 			LunchMenu menu = new LunchMenu(MENSA_NAME);
 			
-			DataInputStream in = new DataInputStream(URL.toURL().openStream());
+			DataInputStream in = new DataInputStream(getURI(date).toURL().openStream());
 
 			Document document = Jsoup.parse(in, null, "");
 			
-			Map<String, Element> lines = document.select("#platocontent > table:nth-child(5) > tbody > tr").stream()
+			LocalDate now = LocalDate.now();
+			int dayOfWeek = date.getDayOfWeek().getValue();
+			if(date.get(IsoFields.WEEK_OF_WEEK_BASED_YEAR) == now.get(IsoFields.WEEK_OF_WEEK_BASED_YEAR)) {
+				// Lunch Menus of days in the past are hidden.
+				dayOfWeek -= now.getDayOfWeek().getValue();
+			}
+			final int child = 5 + 3 * dayOfWeek;
+			Map<String, Element> lines = document.select("#platocontent > table:nth-child(" + child + ") > tbody > tr").stream()
 			.filter(tr -> tr.text().length() > 0)
 			.filter(tr -> tr.child(0).text().startsWith("L"))
 			.collect(Collectors.toMap(tr -> tr.child(0).text(), tr -> tr.child(1).child(0).child(0)));
@@ -67,7 +71,14 @@ public class KITLunchMenuProvider extends AbstractLunchMenuProvider {
 			lines.entrySet().stream().flatMap(e -> {
 				return e.getValue().children().stream()
 						.filter(tr -> {
-							final String text = tr.child(2).text().replace("€", "").replace(",", ".").trim();
+							String text = null;
+							try {
+								text = tr.child(2).text().replaceAll("[^0-9,]", "").replace(",", ".").trim();
+							}
+							catch(IndexOutOfBoundsException ex) {
+								// if line is closed
+								return false;
+							}
 							if(text.length() == 0) {
 								return false;
 							}
@@ -92,7 +103,7 @@ public class KITLunchMenuProvider extends AbstractLunchMenuProvider {
 		
 		LunchMenu menu = new LunchMenu(MENSA_NAME);
 		
-		DataInputStream in = new DataInputStream(FALLBACK_URL.toURL().openStream());
+		DataInputStream in = new DataInputStream(getFallbackURI(date).toURL().openStream());
 
 		Document document = Jsoup.parse(in, null, "");
 		Element table = document.getElementsByTag("tbody").get(0);
@@ -116,5 +127,4 @@ public class KITLunchMenuProvider extends AbstractLunchMenuProvider {
 		
 		return menu;
 	}
-
 }
