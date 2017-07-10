68697c9a7a30391faf3864d20a003596d493379c
Philipp Stehle <philipp.stehle@sap.com>
2017-07-10 21:13:42 +0200

    Add comment field to lunchmenuitem

--- common/src/main/java/de/philipp1994/lunch/common/LunchMenuItem.java
+++ common/src/main/java/de/philipp1994/lunch/common/LunchMenuItem.java
@@ -5,15 +5,21 @@ import java.io.Serializable;
 public class LunchMenuItem implements Serializable {
 	
 	private static final long serialVersionUID = -1796231509545846611L;
-	private final String itemName;
+	private final String itemName, comment;
 	private final double price;
 	
 	public static final double PRICE_UNKOWN = -1;
 
 	public LunchMenuItem(String itemName, double price) {
-		super();
 		this.itemName = itemName;
 		this.price = price;
+		this.comment = null;
+	}
+
+	public LunchMenuItem(String itemName, double price, String comment) {
+		this.itemName = itemName;
+		this.price = price;
+		this.comment = comment;
 	}
 	
 	public String getItemName() {
@@ -24,8 +30,13 @@ public class LunchMenuItem implements Serializable {
 		return price;
 	}
 
+	public String getComment() {
+		return comment;
+	}
+
 	@Override
 	public String toString() {
-		return "LunchMenuItem [itemName=" + itemName + ", price=" + price + "]";
+		return "LunchMenuItem [itemName=" + itemName + ", comment=" + comment + ", price=" + price + "]";
 	}
+
 }
